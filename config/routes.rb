24e31028e46c5d12d2654baed8c99099b368c984
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2019-03-16 16:38:32 -0400

    Add post controllers and specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'dashboard#show'
