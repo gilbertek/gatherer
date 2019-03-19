d9819cb4b0706970f1f38790519f754545bf40f4
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2019-03-18 21:17:43 -0400

    Add some view specs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,53 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = User.new(user_params)
+
+    if @post.save
+      flash[:success] = 'You have successfully created a post.'
+      redirect_to(@post)
+    else
+      render :new
+    end
   end
 
   def show
+    @post = find_post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post = find_post
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:post_id, :title, :text, :publish_date)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :text, presence: true
 end
